dc3af65a01d3555d47e0f2ecfcc94bdd25750d8b
Fatemeh <f.khalleghi.777@gmail.com>
2025-01-18 22:42:16 +0100

    checked and finished

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,9 +1,7 @@
 package se.lexicon;
 
-import se.lexicon.dao.TransactionDao;
-import se.lexicon.dao.WalletDao;
-import se.lexicon.dao.impl.TransactionDaoImpl;
-import se.lexicon.dao.impl.WalletDaoImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.AppConfig;
 import se.lexicon.model.CryptoCurrency;
 import se.lexicon.model.Transaction;
 import se.lexicon.model.Wallet;
@@ -13,23 +11,43 @@ import se.lexicon.service.impl.TransactionManagementImpl;
 import se.lexicon.service.impl.WalletManagementImpl;
 
 import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         //Setup Application
-        //DAO
-        WalletDao walletDao = new WalletDaoImpl();
-        TransactionDao transactionDao = new TransactionDaoImpl();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
         //Services
-        WalletManagement walletManagement = new WalletManagementImpl(walletDao);
-        TransactionManagement management = new TransactionManagementImpl(walletDao, transactionDao);
+        WalletManagement walletManagement = context.getBean(WalletManagementImpl.class);
+        TransactionManagement management = context.getBean(TransactionManagementImpl.class);
+
+
         Wallet simon_wallet = walletManagement.create("Simon's wallet");
+
+
         Transaction transactionBTC1Simon = management.createDepositTransaction(
                 simon_wallet.getId(),
                 CryptoCurrency.BTC,
                 new BigDecimal(1),
                 "Test"
         );
-        System.out.println(transactionBTC1Simon.toString());
+
+        Transaction transactionETH10Simon = management.createDepositTransaction(
+                simon_wallet.getId(),
+                CryptoCurrency.ETH,
+                new BigDecimal(10),
+                "test"
+        );
+
+        Transaction transactionBNB1000Simon = management.createDepositTransaction(
+                simon_wallet.getId(),
+                CryptoCurrency.BNB,
+                new BigDecimal(1000),
+                "test"
+        );
+
+        System.out.println(walletManagement.getById(simon_wallet.getId()));
+
+
     }
 }
--- src/main/java/se/lexicon/dao/impl/WalletDaoImpl.java
+++ src/main/java/se/lexicon/dao/impl/WalletDaoImpl.java
@@ -1,19 +1,32 @@
 package se.lexicon.dao.impl;
 
+import org.springframework.stereotype.Component;
 import se.lexicon.dao.WalletDao;
 import se.lexicon.model.Wallet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+@Component // Making a Bean
 public class WalletDaoImpl implements WalletDao {
+
+    private List<Wallet> walletStorage;
+
+    public WalletDaoImpl() {
+        this.walletStorage = new ArrayList<>();
+    }
 
     @Override
     public Wallet createWallet(Wallet wallet) {
-        return null;
+        walletStorage.add(wallet);
+        return wallet;
     }
 
     @Override
     public Optional<Wallet> findWallet(String id) {
-        return Optional.empty();
+        return walletStorage.stream()
+                .filter( wallet -> wallet.getId().equals(id))
+                .findFirst();
     }
 }
